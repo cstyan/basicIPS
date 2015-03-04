fd4e4471dae347e0940ebe635137d756bddf3215
Callum Styan <callumstyan@gmail.com>
2015-03-04 15:30:36 -0800

    trying slow start

--- ids.js
+++ ids.js
@@ -11,64 +11,57 @@ var timeToCheck = process.argv[2];
 var timeBeforeBan = process.argv[3];
 
 if(!process.argv[2] || !process.argv[3]){
-  console.log("Usage: node ips.js timesToCheckBeforeBanning timeBeforeBanInSeconds");
-  process.exit(1);
+    console.log("Usage: node ips.js timesToCheckBeforeBanning timeBeforeBanInSeconds");
+    process.exit(1);
 }
-
-// Read file for the first time
-/*
-fs.readFile('/var/log/secure', function(error, data){
-  if(error){
-    console.log('Spock Died!');
-
-  } else{
-    bufferString = data.toString();
-
-  }
-
-})
-
-fs.close();
-*/
 
 var logObject = function(ipAddress, primaryDateTimeOfFirstViolation){
     this.ipAddress = ipAddress;
     this.dateTime = [];
-    this.timeToCheck = process.argv[2];
+    //this.timeToCheck = process.argv[2];
 
     this.dateTime[0] = primaryDateTimeOfFirstViolation;
 
     this.addViolation = function(dateTimeOfViolation){
       // we add a violation to the datetime array here
-
       this.dateTime.push(dateTimeOfViolation);
       this.checkIfBan();
     }
 
     this.checkIfBan = function(){
-      // check last 3 date Time's that they violated and see if
-      // we need to ban them
-
-    if(this.dateTime.length > timeToCheck){
-      var lastLog = this.dateTime[this.dateTime.length - 1];
-      var firstLog = this.dateTime[this.dateTime.length - 1 - timeToCheck];
-      var timeDifference = (lastLog - firstLog) / 1000;
-
-        if(timeDifference <= timeBeforeBan){
-          shell.exec("iptables -A INPUT -s " + this.ipAddress + " -j DROP");
-          console.log(this.ipAddress + " got his fucking ass banned bitch!");
+        // check last n date Time's that they violated and see if
+        // we need to ban them
+        if(this.dateTime.length > timeToCheck){
+            var lastLog = this.dateTime[this.dateTime.length - 1];
+            var firstLog = this.dateTime[this.dateTime.length - 1 - timeToCheck];
+            var timeDifference = (lastLog - firstLog) / 1000;
+            //if difference between last attempt and first of range we want to check
+            //is less than the time allowed for failed attempts set by user
+            if(timeDifference <= timeBeforeBan){
+                shell.exec("iptables -A INPUT -s " + this.ipAddress + " -j DROP");
+                console.log(this.ipAddress + " has been banned");
+            } else {
+                //check for slow scan
+                var prev = 0;
+                var numEqual = 0;
+                //check for slow scan/patterns
+                for(var i = 0; i < timeToCheck; i++){
+                    var timeOne = this.dateTime[this.dateTime.length - 1 - i]
+                    var timeTwo = this.dateTime[this.dateTime.length - 1 - (i + 1)]
+                    var timeDifference = (timeOne - timeTwo) / 1000;
+                    //if the previous time difference is equal to this time difference
+                    if(timeDifference == prev){
+                        numEqual++;
+                    }
+                    if(numEqual == timeToCheck){
+                        shell.exec("iptables -A INPUT -s " + this.ipAddress + " -j DROP");
+                        console.log("Slow scan via periodical attempts detected from IP: " + this.ipAddress);
+                        console.log(this.ipAddress + " has been banned");
+                    }
+                    prev = timeDifference;
+                }
+            }
         }
-      }
-
-
-    }
-
-    function banIP(){
-
-    }
-
-    function banIP(xAmountOfTimeToBan){
-      //this is where we run ipTables to ban the IP for X amount of time
     }
 
 }
@@ -76,16 +69,15 @@ var logObject = function(ipAddress, primaryDateTimeOfFirstViolation){
 ft.on('line', function(line){
     bufferString += line;
     if(line.indexOf('Failed') > -1){
-      var ip = line.match(ipRegEx);
+        var ip = line.match(ipRegEx);
 
-      if(!listOfIpAddresses[ip[0]]){
+        if(!listOfIpAddresses[ip[0]]){
 
         var lobj = new logObject(ip[0], new Date());
         listOfIpAddresses[ip[0]] = lobj;
-      }
-      else{
+    } else {
         listOfIpAddresses[ip[0]].addViolation(new Date());
-      }
     }
+}
 
 });
