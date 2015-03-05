a324b58dbda894f4fb65f3cd8891772f3c758410
Callum Styan <callumstyan@gmail.com>
2015-03-04 19:45:29 -0800

    test

--- scheduleOnReboot.rb
+++ scheduleOnReboot.rb
@@ -4,7 +4,7 @@ currentDirectory = Dir.pwd
 #write current crontab to file
 `crontab -l > tempcron`
 #echo new job to temp file
-`echo "@reboot node #{currentDirectory}/ids.js" >>tempcron`
+`echo "@reboot node #{currentDirectory}/ids.js 5 180 1800 1800" >>tempcron`
 #put temp file into crontab
 `crontab tempcron`
 #remove temp file
